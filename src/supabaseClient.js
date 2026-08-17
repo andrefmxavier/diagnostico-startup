ee2d6bc4e71273edfdb73c642e88fd0f80f8e860
andrefmxavier <andrefmxavier@gmail.com>
2026-08-17 19:29:11 -0300

    Create supabaseClient.js

---
+++ src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = 'https://cbfbgsmsvfmpfhdmgpyo.supabase.co/rest/v1/';
+const SUPABASE_ANON_KEY = 'sb_publishable_allHunqunLZpq7--x5rsEg_t3ZY8Yse';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
